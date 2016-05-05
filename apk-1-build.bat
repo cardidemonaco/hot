59a1c827da1b0bf13d3fecc2555f26f81b9fe237
cardidemonaco-work <cardi.demonaco@dynatrace.com>
2016-05-05 12:55:16 -0400

    BAT file update again
    Looks like I will not be able to submit the node_modules folder...

--- apk-1-build.bat
+++ apk-1-build.bat
@@ -1,3 +1,3 @@
-cd "D:\GitHub\cardidemonaco-work hot\hot"
+cd "D:\GitHub\cardidemonaco-work hot\hot\hot"
 "D:\GitHub\cardidemonaco-work hot\hot\node_modules\cordova\bin\cordova" build android --release
 cd ../../
