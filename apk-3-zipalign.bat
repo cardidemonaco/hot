93e6cb22536804382c7b640a6adf28563c4d10ec
cardidemonaco-work <cardi.demonaco@dynatrace.com>
2016-05-05 12:37:27 -0400

    One more BAT file

--- apk-3-zipalign.bat
+++ apk-3-zipalign.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Android\android-sdk\build-tools\22.0.1\zipalign" -v 4 C:\Projects-TortoiseSVN\hot\trunk\hot\platforms\android\build\outputs\apk\android-release-unsigned.apk C:\Projects-TortoiseSVN\hot\trunk\hot\platforms\android\build\outputs\apk\HistoryOfToday.apk
+"C:\Program Files (x86)\Android\android-sdk\build-tools\22.0.1\zipalign" -v 4 "D:\GitHub\cardidemonaco-work hot\hot\hot\platforms\android\build\outputs\apk\android-release-unsigned.apk" "D:\GitHub\cardidemonaco-work hot\hot\hot\platforms\android\build\outputs\apk\HistoryOfToday.apk"
